f49caa54d2c68076b15c1ac8b0c5b6d3ae19c27b
masahitoooki <58727760+masahitoooki@users.noreply.github.com>
2020-11-29 16:27:15 +0900

    Update PurchaseDAO.java

--- shopping/DAO/PurchaseDAO.java
+++ shopping/DAO/PurchaseDAO.java
@@ -7,19 +7,20 @@ import java.sql.PreparedStatement;
 import java.util.List;
 
 public class PurchaseDAO extends DAO {
-	public boolean insert(List<Item> cart, String name, String address) throws Exception {
+	public boolean insert(List<Item> cart, String name, String postal, String address) throws Exception {
 		Connection con=getConnection();
 		con.setAutoCommit(false);
 
 		for (Item item : cart) {
-			PreparedStatement st=con.prepareStatement("insert into purchase values(null, ?, ?, ?, ?, ?, ?)");
+			PreparedStatement st=con.prepareStatement("insert into purchase1 values(null, ?, ?, ?, ?,?, ?,?)");
 			Product1 p=item.getProduct1();
 			st.setInt(1, p.getId());
 			st.setString(2, p.getName());
 			st.setInt(3, p.getPrice());
 			st.setInt(4, item.getCount());
 			st.setString(5, name);
-			st.setString(6, address);
+			st.setString(6, postal);
+			st.setString(7, address);
 			int line=st.executeUpdate();
 			st.close();
 
@@ -37,3 +38,4 @@ public class PurchaseDAO extends DAO {
 		return true;
 	}
 }
+
